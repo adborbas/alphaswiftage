4b71c91ac4bc5ad5bf7e7d2a59f5d541e22ec828
Adam Borbas <adborbas@gmail.com>
2024-03-27 08:26:51 +0100

    Use explicit error description

---
+++ Sources/AlphaSwiftage/Private/logger.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftLogger.swift
+//
+//
+//  Created by Adam Borbas on 23/03/2024.
+//
+
+import Foundation
+import Logging
+import Alamofire
+
+var logger: Logger = Logger(label: "com.adborbas.alphaswiftage")
